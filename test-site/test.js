6eea96b5ec005bfa82bf68d83bca1ccd60d237f2
Jordan Riley <32600970+jcr1593@users.noreply.github.com>
2019-10-30 07:51:50 -0400

    Add files via upload

---
+++ test-site/test.js
@@ -0,0 +1,12 @@
+
+//let myNameArray = ['Chris','Bob', 'Jim'];
+//let myNumberArray = [10, 15, 40];
+
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+multiply(30,40);
+multiply(2,5);
+multiply(6,18);
